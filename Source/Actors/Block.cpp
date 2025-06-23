6c4739051fe08513f2b99967c0143a0cbe5bf313
attiilla <atilappl@gmail.com>
2025-06-23 20:54:48 -0300

    block constructor fix

--- Source/Actors/Block.cpp
+++ Source/Actors/Block.cpp
@@ -9,8 +9,8 @@
 #include "../Components/DrawComponents/DrawPolygonComponent.h"
 #include "../Components/ColliderComponents/AABBColliderComponent.h"
 
-Block::Block(Game* game, const std::string &texturePath, const bool isStatic)
-        :Actor(game)
+Block::Block(Game* game, const std::string &texturePath, const bool isStatic, const bool permeable)
+        :Actor(game),mPermeable(permeable)
 {
     new DrawSpriteComponent(this, texturePath, Game::TILE_SIZE, Game::TILE_SIZE, 10);
     mColliderComponent = new AABBColliderComponent(this, 0, 0, Game::TILE_SIZE, Game::TILE_SIZE, ColliderLayer::Blocks, isStatic);
